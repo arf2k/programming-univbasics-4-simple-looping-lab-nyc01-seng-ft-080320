4ec98bba6e87b608d564644a5a1c53bef561edad
Alex Foreman <foreman.alexander@gmail.com>
2020-07-03 19:22:32 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,8 +23,7 @@ end
 def return_string_array(array)
   counter = 0 
   while counter < array.length do 
-    array[counter].to_s 
     counter += 1 
-    return array 
+    return array[counter].to_s 
   end
 end 
