cac5753590245a571df5ec638e1b71c2eb788903
Alex Foreman <foreman.alexander@gmail.com>
2020-07-03 19:06:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,11 @@ def loop_message_five_times(string)
     counter += 1 
   end
 end 
+
+def loop_message_n_times(string, number)
+  counter = 0 
+  while counter < number.length 
+    puts string
+    counter += 1 
+  end
+end 
